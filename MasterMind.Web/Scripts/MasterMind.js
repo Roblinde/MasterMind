c8e5ddf26672dda437f89e17c930ce805bef859f
Robert Linde <robert@selberglinde.se>
2012-06-11 22:15:30 +0200

    Added visual representation of color selection

--- MasterMind.Web/Scripts/MasterMind.js
+++ MasterMind.Web/Scripts/MasterMind.js
@@ -1,14 +1,25 @@
 ﻿/// <reference path="jquery-1.6.4.js" />
 
-//temporary
+//temporary arrays, will be moved to a js class
 var selected = new Array();
+var colors = new Array('white', 'yellow', 'orange', 'red', 'purple', 'teal', 'green', 'black');
+
 
 $(function () {
 
     var hub = $.connection.masterMind;
 
     $('#ColorColumn .colorBox').click(function () {
-        selected.push($(this).index());
+
+        var indexClicked = $(this).index();
+        //store selection
+        selected.push(indexClicked);
+        
+        var lastEmptyRow = $('.none').last().parent();
+
+        //add selection visually
+        //will be changed to drag and drop
+        lastEmptyRow.find('.none').first().addClass(colors[indexClicked]).removeClass('none');
 
         if (selected.length == 4) {
             
@@ -24,7 +35,13 @@ $(function () {
     hub.showGuess = function (guess, connectionId) {
 
         if ($.connection.hub.id != connectionId) {
-            alert('Another player guessed: ' + guess[0] + guess[1] + guess[2] + guess[3]);
+
+            var lastEmptyRow = $('.none').last().parent();
+            //add the guess visually
+            lastEmptyRow.find('.none').first().addClass(colors[guess[0]]).removeClass('none');
+            lastEmptyRow.find('.none').first().addClass(colors[guess[1]]).removeClass('none');
+            lastEmptyRow.find('.none').first().addClass(colors[guess[2]]).removeClass('none');
+            lastEmptyRow.find('.none').first().addClass(colors[guess[3]]).removeClass('none');
         }
     }
 
